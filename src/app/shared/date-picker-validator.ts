a2ea8701e8e95cc84633761e259791e9009d6db5
tincholv14 <martinlerda21@gmail.com>
2020-05-06 14:16:37 -0300

    1 file modified, 3 added, 32 deleted

--- src/app/shared/date-picker-validator.ts
+++
@@ -1,8 +0,0 @@
-import { AbstractControl } from '@angular/forms';
-
-export function DatepickerValidator(control: AbstractControl) {
-  if (!control.value) {
-    return { dateValid: true };
-  }
-  return null;
-}
